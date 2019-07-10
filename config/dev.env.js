4e9e52de89f06f98c6bef8bbdf7ceb39a44f9dcd
lgzkd1990 <wodeshijie1234@126.com>
2019-07-10 19:23:26 +0800

    Initial commit

---
+++ config/dev.env.js
@@ -0,0 +1,8 @@
+'use strict'
+const merge = require('webpack-merge')
+const prodEnv = require('./prod.env')
+
+module.exports = merge(prodEnv, {
+  NODE_ENV: '"development"',
+  BASE_API: '"http://127.0.0.1:7300/mock/5d25a9dc654b8f288474515e"',
+})
